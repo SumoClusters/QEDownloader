605fecc4c24f3c17d5db19cfdea7a029cf8572bd
harry <werewaltz@gmail.com>
2015-08-31 16:17:02 -0600

    first commit

--- QEDownloader.py
+++ QEDownloader.py
@@ -1,12 +1,16 @@
-__author__ = 'harry'
-
+__author__ = 'SumoClusters'
+# from PyPDF2 import PdfFileMerger, PdfFileReader
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 from pyvirtualdisplay import Display
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from os.path import expanduser
 import re
 import time
-
+import os
+import platform
+import getpass
 
 def main():
     print("         Welcome to Questa Downloader!")
@@ -14,12 +18,22 @@ def main():
     print("We will need your Login credentials to proceed.")
     print("  We will destroy the credentials upon use, don't worry.")
     username = input("Login: ")
-    password = input("Password: ")
+    password = (getpass.getpass('Please Enter a Password: '))
     whatclass = int(input("Type what row your class is listed:  "))
-    # reclass = re.escape(whatclass)
-    # display = Display(visible=0, size=(800, 600))
-    # display.start()
-    driver = webdriver.Firefox()
+    whatclass -= 1
+    home = expanduser("~")
+    operatingsystem = platform.system()
+    display = Display(visible=0, size=(800, 600))  # These two lines make the browser invisible.
+    display.start()
+    filen = 'a'  # base filename
+    filec = 0  # filename counter
+    filep = filen+str(filec)  # perfect filename
+    bstring = 'Print___'
+    print("Configuring Firefox Profile")
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("print.always_print_silent", True)
+    print("Starting Firefox")
+    driver = webdriver.Firefox(profile)
     print("Travelling to Distance Education ITT Tech")
     driver.get('http://distance-education.itt-tech.edu/')
     login = driver.find_element_by_name("fblogincd")
@@ -48,19 +62,70 @@ def main():
     preg = re.compile('\s\d{3}\s')
     pages = soup.find(text=preg)
     non_decimal = re.compile(r'\D+')
-    truepages = re.sub(non_decimal, '', pages)
+    truepages = int(re.sub(non_decimal, '', pages))
     print(truepages)
     driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL + 'w')
-    """
-    print("Starting to print the pages")
+
+    print("Initializing Downloading Loop")
     # after amazon gets done with finding page count
     driver.switch_to.window(driver.window_handles[-1])
     driver.find_element_by_class_name("CLiKSLpNrmlLnk_Ebook").click()
     time.sleep(8)
     driver.switch_to.window(driver.window_handles[-1])
-    for i in pages:
-        begin = 0
-        driver.find_element_by_css_selector("button.submit_lt_gray.print-button").click()
+    begin = 0
+    end = 10
+    filen = 'a'  # base filename
+    filec = 0  # filename counter
+    filep = filen + str(filec) + ".pdf"  # perfect filename
+    # The following will open the book in the main browser by using a CTRl + Left Click
+    newtab = driver.find_element_by_xpath("//a[@class='solo root go']")
+    ActionChains(driver) \
+        .key_down(Keys.CONTROL) \
+        .click(newtab) \
+        .key_up(Keys.CONTROL) \
+        .perform()
+    driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL + 'w')
+    driver.switch_to.window(driver.window_handles[-1])
+    driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL + 'w')
+    pdf = home + "/PDF"
+    if not os.path.exists(pdf):
+        os.mkdir(pdf)
+    os.chdir(pdf)  # goes to PDF directory
+    time.sleep(10)
+    loop = truepages - 5
+
+    if operatingsystem == 'Linux':
+        while begin < loop:
+            driver.switch_to.window(driver.window_handles[-1])
+            print("Downloading section: " + str(begin) + " of " + str(end) + ". Out of: " + str(truepages))
+            driver.find_element_by_xpath("//button[@ga-label='Open Print Button']").click()
+            driver.find_element_by_xpath("//input[@id='print_from']").clear()
+            driver.find_element_by_xpath("//input[@id='print_from']").send_keys(begin)
+            driver.find_element_by_xpath("//input[@id='print_to']").clear()
+            driver.find_element_by_xpath("//input[@id='print_to']").send_keys(end)
+            # Starts the print process
+            driver.find_element_by_xpath("//button[contains(@class,'submit print_button')]").click()
+            time.sleep(12)
+            driver.switch_to.window(driver.window_handles[-1])
+            driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL + 'w')
+            for filename in os.listdir(pdf):
+                if filename.startswith(bstring):
+                    os.rename(filename, filep)
+
+            filec += 1
+            filep = filen + str(filec) + ".pdf"
+            begin += 10
+            end += 10
+""""
+   doesn't work I Recommend third party software to merge the the pdfs
+    print("Merging to PDF")
+    merger = PdfFileMerger()
+    files = [x for x in os.listdir(pdf) if x.endswith(".pdf")]
+    for fname in sorted(files):
+        merger.append(PdfFileReader(open(os.path.join(pdf, fname), 'rb')))
+    merger.write(booktitle+'.pdf')
+    name = booktitle+'.pdf'
+    print("Extracting Headers from penultimate PDF")
 """
 if __name__ == '__main__':
     main()
@@ -76,4 +141,13 @@ if __name__ == '__main__':
     https://addons.mozilla.org/en-US/firefox/addon/fastest-search/
     http://www.amazon.com/Getting-Started-Beautiful-Soup-Vineeth/dp/1783289554/ref=sr_1_1?ie=UTF8&qid=1440788739&sr=8-1&keywords=getting+started+with+beautiful+soup
     https://stackoverflow.com/questions/22650506/how-to-get-rid-of-non-alphanumeric-characters-at-the-beginning-or-end-of-a-strin
+    https://stackoverflow.com/questions/15954682/setting-selenium-to-use-custom-profile-but-it-keeps-opening-with-default
+    https://stackoverflow.com/questions/20984698/use-firefox-print-or-save-as-features-using-selenium-webdriver
+    https://addons.mozilla.org/en-us/firefox/addon/element-locator-for-webdriv/
+    https://stackoverflow.com/questions/27775759/send-keys-control-click-selenium
+    http://michaeljaylissner.com/posts/2008/10/19/change-the-default-print-to-file-to-pdf-in-ubuntu-hardy/
+    https://stackoverflow.com/questions/4028904/how-to-get-the-home-directory-in-python
+    https://stackoverflow.com/questions/2759067/rename-files-in-python
+    https://stackoverflow.com/questions/273192/in-python-check-if-a-directory-exists-and-create-it-if-necessary
+    https://www.binpress.com/tutorial/manipulating-pdfs-with-python/167
 """
---
+++ README.md
@@ -0,0 +1,17 @@
+Welcome to Questa Ebook Downloader!
+
+I created this program because I was fed up with ITT's online ebook reader.
+I couldn't get the merge pdf section to work out; for some reason it would join them out
+of order. It should be noted that it is only tested and supported for Linux based OS.
+-------------------------
+To get started
+
+From your console
+    sudo chmod +x setup
+    sudo ./setup
+    python QEDownloader.py
+
+___________________________
+
+What to expect on first run
+
